5fac486d26206306a81270c28c81585eb4d93d3a
iliophanis <iliophanis@gmail.com>
2020-01-29 15:09:31 +0200

    section 4 cqrs

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Application.Activities;
@@ -21,5 +22,34 @@ namespace API.Controllers
         public async Task<ActionResult<List<Activity>>> List(){
             return await _mediator.Send(new List.Query());
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> Details(Guid id)
+        {
+            return await _mediator.Send(new Details.Query{Id=id});
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult<Unit>> Create(Create.Command command)
+        {
+            return await _mediator.Send(command);
+        }
+
+        [HttpPut("{id}")]
+
+          public async Task<ActionResult<Unit>> Edit(Guid id,Edit.Command command)
+        {
+            command.Id=id;
+            return await _mediator.Send(command);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Unit>> Delete(Guid id)
+        {
+            
+            return await _mediator.Send(new Delete.Command{Id=id});
+        }
+        
     }
 }
